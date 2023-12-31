7bff04d5823d28d54c39edbc62b884a2374d4da6
John Shaughnessy <john@johnshaughnessy.com>
2023-12-30 20:36:34 -0500

    wip supervisor

--- supervisor/README.md
+++ supervisor/README.md
@@ -2,7 +2,7 @@
 
 A python (Flask) web server that allows clients to query and change the kubernetes cluster.
 
-## Instructions
+## Setup
 
 Install `kubectl`, `docker`, `gcloud`.
 
@@ -10,16 +10,24 @@ Update the configuration file `./config.ini`.
 
 Update the registry config map: `registry-config.ini`.
 
-Apply the ConfigMap: `kubectl apply -f registry-config.yaml`
+Log in to `gcloud`: `glcoud auth login`.
 
-Perform one-time docker configuration (Replace `us-central1-docker.pkg.dev` with your artifact registry location):
-
+Perform one-time docker configuration:
 ```sh
 gcloud auth configure-docker us-central1-docker.pkg.dev
 ```
+(Replace `us-central1-docker.pkg.dev` with your artifact registry location)
+
 
 Build and upload the docker image with `./build_and_upload.sh`.
 
-Apply the deployment (first time only): `kubectl apply -f supervisor-deployment.yaml`
+Apply the ConfigMap: `kubectl apply -f registry-config.yaml`
+Apply the deployment: `kubectl apply -f supervisor-deployment.yaml`
+
+Run/replace the live deployment with `./update_k8s_deployment.sh`.
+
+## Update
+
+Build and upload the docker image with `./build_and_upload.sh`.
 
 Run/replace the live deployment with `./update_k8s_deployment.sh`.
--- supervisor/update_k8s_deployment.sh
+++ supervisor/update_k8s_deployment.sh
@@ -3,13 +3,32 @@
 # Load configuration
 source <(grep = config.ini | sed 's/ *= */=/g')
 
+# Verify the current Kubernetes context
+current_context=$(kubectl config current-context)
+if [ "$current_context" != "$context_name" ]; then
+    echo "Error: Current context ($current_context) does not match the expected context ($context_name)."
+    exit 1
+fi
+
+
+# Check if the namespace exists
+if kubectl get namespace "$namespace" > /dev/null 2>&1; then
+    echo "Namespace $namespace already exists."
+else
+    echo "Creating namespace $namespace..."
+    kubectl create namespace "$namespace"
+fi
+echo "Ensuring that the registry-config ConfigMap exists..."
+kubectl apply -f registry-config.yaml --namespace=${namespace}
+echo "Ensuring that the supervisor-deployment Deployment exists..."
+kubectl apply -f supervisor-deployment.yaml --namespace=${namespace}
+
+# Updating Kubernetes Deployment
+echo "Updating Kubernetes Deployment..."
 # Set variables
 IMAGE_NAME="osai-kube/supervisor"
 TAG="latest"
 FULL_IMAGE_NAME="${artifact_registry}/${IMAGE_NAME}:${TAG}"
-
-# Updating Kubernetes Deployment
-echo "Updating Kubernetes Deployment..."
 kubectl set image deployment/${deployment_name} supervisor="${FULL_IMAGE_NAME}" --namespace=${namespace}
 
 echo "Deployment update complete."
