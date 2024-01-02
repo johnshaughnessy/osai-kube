595eaaa78dc3336e597362584a8d825a86ab3ec6
John Shaughnessy <john@johnshaughnessy.com>
2024-01-02 16:25:36 -0500

    update logs.sh

--- scripts/logs.sh
+++ scripts/logs.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-# Load configuration
-source <(grep = config.ini | sed 's/ *= */=/g')
+SCRIPT_DIR="$(dirname "$0")"
+source "$SCRIPT_DIR/set_environment_variables.sh"
 
-# Verify the current Kubernetes context
-current_context=$(kubectl config current-context)
-if [ "$current_context" != "$context_name" ]; then
-    echo "Error: Current context ($current_context) does not match the expected context ($context_name)."
+# Function to display messages
+print_message() {
+    if [ "$2" == "ERROR" ]; then
+        # Red color for error
+        [ "$NO_COLOR" == "1" ] && echo "[osai-kube] [$2] $1" || echo -e "[osai-kube] [\033[0;31m$2\033[0m] $1"
+    elif [ "$2" == "OK" ]; then
+        # Green color for OK
+        [ "$NO_COLOR" == "1" ] && echo "[osai-kube] [$2] $1" || echo -e "[osai-kube] [\033[0;32m$2\033[0m] $1"
+    else
+        # No color for other messages
+        echo "[osai-kube] [$2] $1"
+    fi
+}
+
+# Set $LABEL based on the first command line argument
+if [ -z "$1" ]; then
+    print_message "Target required: (supervisor | gpu-diagnostic-pod | doodle | chat)" "ERROR"
     exit 1
+
+elif [ "$1" == "gpu-diagnostic-pod" ]; then
+    POD_NAME="gpu-diagnostic-pod"
+else
+    LABEL="app=$1"
+    POD_NAME=$(kubectl get pods -l $LABEL -o jsonpath="{.items[0].metadata.name}" 2> /dev/null)
 fi
 
-# Check if the namespace exists
-if ! kubectl get namespace "$namespace" > /dev/null 2>&1; then
-    echo "Namespace $namespace does not exist."
-    exit 1
-fi
-
-# Set the namespace and correct label
-NAMESPACE="osai-kube"
-LABEL="app=supervisor"  # Replace with the actual label key-value
-LABEL="app=doodle"  # Replace with the actual label key-value
-
-# Find a pod with the given label
-POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l $LABEL -o jsonpath="{.items[0].metadata.name}")
-
-# Check if a pod name was found
 if [ -z "$POD_NAME" ]; then
-    echo "No pod found with label $LABEL in namespace $NAMESPACE."
+    echo "No pod found for $1."
     exit 1
 fi
 
