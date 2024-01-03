15bf0d4372496f34fe1bcbea1c88ad2a0ad6c887
John Shaughnessy <john@johnshaughnessy.com>
2024-01-02 20:46:29 -0500

    Update deployment yaml files directly on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,9 @@
 SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/set_environment_variables.sh"
 
+CONFIG_DIR="$SCRIPT_DIR/../kubernetes-configs/"
+
+
 # Function to display messages
 print_message() {
     if [ "$2" == "ERROR" ]; then
@@ -17,6 +20,9 @@ print_message() {
     elif [ "$2" == "CREATED" ]; then
         # Green color for created
         [ "$NO_COLOR" == "1" ] && echo "[osai-kube] [$2] $1" || echo -e "[osai-kube] [\033[0;32m$2\033[0m] $1"
+    elif [ "$2" == "UPDATED" ]; then
+        # Green color for updated
+        [ "$NO_COLOR" == "1" ] && echo "[osai-kube] [$2] $1" || echo -e "[osai-kube] [\033[0;32m$2\033[0m] $1"
     elif [ "$2" == "CONFIGURED" ]; then
         # Green color for configured
         [ "$NO_COLOR" == "1" ] && echo "[osai-kube] [$2] $1" || echo -e "[osai-kube] [\033[0;32m$2\033[0m] $1"
@@ -26,7 +32,7 @@ print_message() {
     fi
 }
 
-apply_k8s_config() {
+k8s_apply_config() {
     output=$(kubectl apply -f $1 2>&1)
     result=$?
 
@@ -64,15 +70,42 @@ else
     fi
 fi
 
-CONFIG_DIR="$SCRIPT_DIR/../kubernetes-configs/"
+update_deployment_image() {
+    local deployment_file="$1"
+    local filename_with_parent=$(echo $deployment_file | sed "s|$CONFIG_DIR||g")
+    local image_name="$2"
+
+    local image_digest=$(gcloud container images list-tags "$ARTIFACT_REGISTRY/$image_name" --limit=1 --sort-by=~TIMESTAMP --format="json" | jq -r ".[0].digest")
+    local full_image="\$(ARTIFACT_REGISTRY)/${image_name}@${image_digest}"
+
+    # Make a backup of the original file for comparison
+    cp "$deployment_file" "${deployment_file}.bak"
+
+    # Update the deployment YAML file
+    sed -i "s|image: .*|image: $full_image|" "$deployment_file"
+
+    # Check if the file was changed
+    if cmp -s "${deployment_file}.bak" "$deployment_file"; then
+        print_message "No change to $image_name in $filename_with_parent." "UNCHANGED"
+    else
+        print_message "Updated $image_name in $filename_with_parent to $image_digest." "UPDATED"
+    fi
+
+    # Remove the backup file
+    rm "${deployment_file}.bak"
+}
+
+SUPERVISOR_DEPLOYMENT_FILE="$CONFIG_DIR/deployments/supervisor-deployment.yaml"
+SUPERVISOR_IMAGE_NAME="osai-kube/supervisor"
+update_deployment_image "$SUPERVISOR_DEPLOYMENT_FILE" "$SUPERVISOR_IMAGE_NAME"
+
+DOODLE_DEPLOYMENT_FILE="$CONFIG_DIR/deployments/doodle-deployment.yaml"
+DOODLE_IMAGE_NAME="browserlab/doodle"
+update_deployment_image "$DOODLE_DEPLOYMENT_FILE" "$DOODLE_IMAGE_NAME"
 
 configs=$(find $CONFIG_DIR -type f -name "*.yaml")
 
 for config in $configs; do
-    apply_k8s_config $config
+    k8s_apply_config $config
 done
 
-SUPERVISOR_IMAGE_DIGEST=$(gcloud container images list-tags "$ARTIFACT_REGISTRY/osai-kube/supervisor" --limit=1 --sort-by=~TIMESTAMP --format="json" | jq -r ".[0].digest")
-SUPERVISOR_IMAGE="${ARTIFACT_REGISTRY}/osai-kube/supervisor@$SUPERVISOR_IMAGE_DIGEST"
-print_message "Updating supervisor image to $SUPERVISOR_IMAGE." "INFO"
-kubectl set image deployment/supervisor-deployment supervisor="${SUPERVISOR_IMAGE}"
